0325f465fed658cd81877fb850067aa79963435c
Vincent Peng <vincent@vincentpeng.me>
2017-01-10 17:40:14 +1100

    Initial Commit

---
+++ Ipify/Ipify.h
@@ -0,0 +1,19 @@
+//
+//  Ipify.h
+//  Ipify
+//
+//  Created by Vincent Peng on 1/10/17.
+//  Copyright © 2017 Vincent Peng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Ipify.
+FOUNDATION_EXPORT double IpifyVersionNumber;
+
+//! Project version string for Ipify.
+FOUNDATION_EXPORT const unsigned char IpifyVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Ipify/PublicHeader.h>
+
+
