266775cbb82ae8e206af9e528d5650ac3554e4f2
Vincent Peng <vincent@vincentpeng.me>
2017-01-11 17:14:12 +1100

    Add tests for Ipify module

--- IpifyTests/IpifyTests.swift
+++ IpifyTests/IpifyTests.swift
@@ -32,5 +32,39 @@ class IpifyTests: XCTestCase {
             // Put the code you want to measure the time of here.
         }
     }
-    
+	
+	func testGetPublicIPAddress() {
+		let expectation = self.expectation(description: "Query public IP address from api.ipify.org should succeed")
+
+		Ipify.getPublicIPAddress { result in
+			switch result {
+			case .success(let ip):
+				XCTAssertTrue(self.validateIpAddress(ipToValidate: ip), "\(ip) should be a valid IP address")
+				
+			case .failure(_):
+				XCTFail("Fail to query IP address from the server")
+			}
+			
+			expectation.fulfill()
+		}
+		
+		waitForExpectations(timeout: 20.0, handler: nil)
+	}
+	
+	
+	private func validateIpAddress(ipToValidate: String) -> Bool {
+		var sin = sockaddr_in()
+		var sin6 = sockaddr_in6()
+		
+		if ipToValidate.withCString({ cstring in inet_pton(AF_INET6, cstring, &sin6.sin6_addr) }) == 1 {
+			// IPv6 peer.
+			return true
+		}
+		else if ipToValidate.withCString({ cstring in inet_pton(AF_INET, cstring, &sin.sin_addr) }) == 1 {
+			// IPv4 peer.
+			return true
+		}
+		
+		return false;
+	}
 }
